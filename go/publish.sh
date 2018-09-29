01e9d239e4537ae38baaf73f6f78c83fe81e6d2f
lance7in <zexpp5@gmail.com>
2018-09-29 18:22:15 +0800

    publicate and backup 2018-09-29,18:09:1538216535

--- go/publish.sh
+++ go/publish.sh
@@ -2,12 +2,12 @@
 cd `dirname $0`
 cd ..
 hexo clean && hexo g && hexo d -g
-echo -e "\033[46;37m \n pulication done \n \033[0m"
-echo -e "\033[46;37m \n backup config \n \033[0m"
+echo  "\033[46;37m \n pulication done \n \033[0m"
+echo  "\033[46;37m \n backup config \n \033[0m"
 cp themes/next/_config.yml go/backup/
 git add *
 current_dateTime="`date +%Y-%m-%d,%H:%m:%s`"
 git commit -m "publicate and backup $current_dateTime"
 git push
-echo -e "\033[46;37m \n backup done \n \033[0m"
+echo  "\033[46;37m \n backup done \n \033[0m"
 
